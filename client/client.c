67ed704860871d0a700f812c20d41217fc62cc28
Henry Stratmann <henry3writes@gmail.com>
2010-11-03 17:14:28 -0500

    Implemented more features for the gui.  In the SPEC file I put a '>' chracter in front of the key commands that are done.

--- client/client.c
+++ client/client.c
@@ -8,6 +8,7 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
@@ -21,15 +22,21 @@
 #define MAX_COLUMNS 					40
 #define MAX_CHARS_IN_CLIENT_TYPING_WINDOW 	MAX_ROWS * MAX_COLUMNS
 
+#define TRANSCRIPT_MAX_ROWS				23
+#define TRANSCRIPT_MAX_COLUMNS				40
+#define MAX_CHARS_IN_TRANSCRIPT_WINDOW		TRANSCRIPT_MAX_ROWS * TRANSCRIPT_MAX_COLUMNS
 
 
 
-char client_buffer[1024 * 4];
+char client_buffer[1024];
 int client_current_line = 0;
+
+char transcript_buffer[1024 * 4];
+int transcript_current_line = 0;
 
 WINDOW *transcript_window;
 WINDOW *client_chat_window;
-
+WINDOW *other_chat_windows[9];
 
 
 /* Get the size of the current terminal window. */
@@ -46,30 +53,84 @@ static void get_terminal_size(int *x, int *y)
 }
 
 
-/* Print out the current client chat-typing window. */
-static void print_client_chat_buffer()
+/* Page the specified window up by "line" number of lines. */
+void window_page_up(WINDOW *win, int *line, int max_columns, char *buffer)
+{
+	if((*line)*max_columns > strlen(buffer)) (*line) --;
+
+	wclear(win);
+	wprintw(win, &buffer[(*line)*max_columns]);
+}
+
+/* Page the specified window down by "line" number of lines. */
+void window_page_down(WINDOW *win, int *line, int max_columns, char *buffer)
+{
+	if((*line) < 0) (*line) = 0;
+
+	wclear(win);
+	wprintw(win, &buffer[(*line)*max_columns]);
+}
+
+
+
+/* Print out the current buffer into the specified window.
+ *
+ * win		 ~ The ncurses window to print to.
+ * max_chars	 ~ The max number of characters that can appear in the windows view at any given time.
+ * max_columns   ~ The max number of characters that can be in a given column.
+ * buffer	 ~ The character buffer to write to the window.
+ * curr_line	 ~ The current line the user is at in the window.
+ */
+static void print_buffer_to_window(WINDOW *win, int max_chars, int max_columns, char *buffer, int *curr_line)
 {
 	/* Clear the chat window for writing. */
-	wclear(client_chat_window);
+	wclear(win);
 
 	/* Here, we need to figure out how much text is already in our chat window.
 	 * The reason is because every time we want to add a new character to our 
 	 * window, we don't want it to scroll to the top.  We want our window to stay
 	 * were it's at. */
-	if( (strlen(client_buffer) - MAX_CHARS_IN_CLIENT_TYPING_WINDOW) > 0 ) {
-		int num_chars_off_screen =  strlen(client_buffer) - MAX_CHARS_IN_CLIENT_TYPING_WINDOW;
-		client_current_line = 0;
+	if( (strlen(buffer) - max_chars) > 0 ) {
+		int num_chars_off_screen =  strlen(buffer) - max_chars;
+		(*curr_line) = 0;
 
 		while(num_chars_off_screen > 0) {
-			client_current_line ++;
-			num_chars_off_screen -= MAX_COLUMNS;
+			(*curr_line) ++;
+			num_chars_off_screen -= max_columns;
 		}
 
-		wprintw(client_chat_window, &client_buffer[client_current_line*MAX_COLUMNS]);
+		wprintw(win, &buffer[(*curr_line)*max_columns]);
 	} else {
-		wprintw(client_chat_window, client_buffer);
+		wprintw(win, buffer);
 	}
 }
+
+/* Print out the current client chat-typing window. */
+static void print_client_chat_buffer()
+{
+	print_buffer_to_window( client_chat_window, 
+				MAX_CHARS_IN_CLIENT_TYPING_WINDOW,
+				MAX_COLUMNS,
+				client_buffer,
+				&client_current_line );
+}
+
+/* Print out the current client chat-typing window. */
+static void print_transcript_chat_buffer()
+{
+	print_buffer_to_window( transcript_window, 
+				MAX_CHARS_IN_TRANSCRIPT_WINDOW,
+				TRANSCRIPT_MAX_COLUMNS,
+				transcript_buffer,
+				&transcript_current_line );
+}
+
+
+/*
+ =================================================================
+ =================================================================
+ =================================================================
+ */
 
 
 /* Get the text inside our chat window. */
@@ -89,7 +150,12 @@ void clear_text_from_client_typing_window(void)
 /* Add some text to our transcript window. */
 void write_to_transcript_window(char *text)
 {
-	wprintw(transcript_window, text);
+	if( (strlen(transcript_buffer) + strlen(text)) >= sizeof(transcript_buffer) ) {
+		/* TODO: handle buffer overrun */
+	}
+
+	strcat(transcript_buffer, text);
+	print_transcript_chat_buffer();
 }
 
 
@@ -98,9 +164,11 @@ int main(int argc, char* argv[])
 {
 	int is_running = 1;
 	int x_terminal_size, y_terminal_size;
-        int client_id = init_client();                  /* create a client. */
+/*        int client_id = init_client();                   create a client. */
 	
         memset(client_buffer, '\0', sizeof(client_buffer));
+	memset(transcript_buffer, '\0', sizeof(transcript_buffer));
+
 	get_terminal_size(&x_terminal_size, &y_terminal_size);
 	
 	initscr();
@@ -111,32 +179,66 @@ int main(int argc, char* argv[])
 	transcript_window  = newwin(23,40,0,0);
 	client_chat_window = newwin(MAX_ROWS,MAX_COLUMNS,20,40);
 
-	write_to_transcript_window("Hello!\n");
-	write_to_transcript_window("Hey all!\nI love BlackChat, it's so awesome!\n");
-	write_to_transcript_window("Press the \'q\' key to quit!");
+	write_to_transcript_window("**************************************\n");
+	write_to_transcript_window("******** Wecome to BlackChat! ********\n");
+	write_to_transcript_window("**************************************\n");
 
 	while(is_running) {
 		int ch = getch();
+/*		char buf[512];
+
+		sprintf(buf, "key pressed: '%c'  int value: %d\n", ch, ch);
+		write_to_transcript_window(buf);
+*/
+
 		switch(ch) {
-			case 'q': 
-				is_running = 0;
-				break;
+			/*
+			 * Check if we pressed a control key. */
+			if(iscntrl(ch)) {
+				case 8:  /* CTRL-H */
+					client_buffer[ strlen(client_buffer)-1 ] = '\0';
+					print_client_chat_buffer();
+					break;
+				case 10: /* CTRL-J and CTRL-M */
+					write_to_transcript_window("[Client Says]: ");    /* write client chat text to transcript window. */
+					write_to_transcript_window(client_buffer);
+					write_to_transcript_window("\n");
+					clear_text_from_client_typing_window();
+					break;
+				case 14: /* CTRL-N */
+					transcript_current_line ++;
+					window_page_up( transcript_window,
+							&transcript_current_line,
+							TRANSCRIPT_MAX_COLUMNS,
+							transcript_buffer );
+					break;
+				case 16: /* CTRL-P */
+					transcript_current_line --;
+					window_page_down( transcript_window,
+							  &transcript_current_line,
+							  TRANSCRIPT_MAX_COLUMNS,
+							  transcript_buffer );
+					break;
+				case 17: /* CTRL-Q */ 
+					is_running = 0;
+					break;
+			}
 
 			/* Scroll the clients typing window down. */
 			case KEY_DOWN:
 				client_current_line ++;
-				if(client_current_line*MAX_COLUMNS > strlen(client_buffer)) client_current_line --;
-
-				wclear(client_chat_window);
-				wprintw(client_chat_window, &client_buffer[client_current_line*MAX_COLUMNS]);
+				window_page_up( client_chat_window, 
+					        &client_current_line, 
+						MAX_COLUMNS,
+						client_buffer );
 				break;
 			/* Scroll the clients typing window up. */
 			case KEY_UP:
 				client_current_line --;
-				if(client_current_line < 0) client_current_line = 0;
-
-				wclear(client_chat_window);
-				wprintw(client_chat_window, &client_buffer[client_current_line*MAX_COLUMNS]);
+				window_page_down( client_chat_window,
+						  &client_current_line,
+						  MAX_COLUMNS,
+						  client_buffer );
 				break;
 		
 			/* Delete the previous chracter. */
@@ -151,7 +253,7 @@ int main(int argc, char* argv[])
 				/* Store the new char in our buffer. */
 				client_buffer[ strlen(client_buffer) ] = ch;
 				
-				/* Print our new chat buffer. */
+				/* Print our new/updated buffer. */
 				print_client_chat_buffer();
 				break;
 		}
@@ -164,7 +266,7 @@ int main(int argc, char* argv[])
 	delwin(client_chat_window);
 	endwin();
         
-        close_client(client_id);
+      /*  close_client(client_id); */
 
 	return 0;
 }
