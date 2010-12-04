833ec29f995cc19bd55f6f730e1d7d32151ba0f5
Joshua Hartman <jh@grok.MissouriState.edu>
2010-12-04 13:28:48 -0600

    added some more to the client-server functions

--- client/clientsocket.h
+++ client/clientsocket.h
@@ -7,7 +7,10 @@
 
 
 /* Creates a client socket and returns the file descriptor for it. */
-int init_client(void);
+int init_client(char *name);
+
+/* initializes the client side user list */
+void init_user_list(int client_id);
 
 /* Closes a client socket.  You must pass in the file descriptor created by init_client */
 void close_client(int client_id);
